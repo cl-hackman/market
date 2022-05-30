31f30ca0ad4767b72651228e1a525e70b80601e7
Collins <96515902+cl-hackman@users.noreply.github.com>
2022-05-30 12:31:23 -0400

    Update application.py

--- flask_project/market/application.py
+++ flask_project/market/application.py
@@ -1,7 +1,7 @@
-from market import application #can be app not needed
+from market import app #can be app not needed
 
 # to check if this file has executed without importing from the package file (__init__)
 if __name__ == '__main__': 
-    application.run('localhost',5000,debug=True) # To run the app, (debug=True) skips all the setup envs (set FLASK_APP=file.py, set FLASK_DEBUG=1, flask run)
+    app.run('localhost',5000,debug=True) # To run the app, (debug=True) skips all the setup envs (set FLASK_APP=file.py, set FLASK_DEBUG=1, flask run)
 
 #('localhost',5000): beanstalk needs this identifier to run it other than on the standard http PORT 80
